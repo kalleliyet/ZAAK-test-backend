8d9684cf5f2ff416e0c9002d166e02539e45658e
kalleliyet <mohamedazizkallel2001@gmail.com>
2026-04-07 17:41:42 +0100

    initializing spring project

---
+++ src/main/java/ZAAK/backend/ZAAK_Test/ZaakTestApplication.java
@@ -0,0 +1,13 @@
+package ZAAK.backend.ZAAK_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaakTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaakTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZAAK/backend/ZAAK_Test/ZaakTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ZAAK.backend.ZAAK_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaakTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
